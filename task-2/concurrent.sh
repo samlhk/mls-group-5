42089fed10e9029cedde5f6b80a31a26793a2258
Sam Lee <s2157573@uhtred.inf.ed.ac.uk>
2025-04-11 01:58:15 +0100

    update tests

--- task-2/concurrent.sh
+++ task-2/concurrent.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-URL="http://192.168.47.132:8000/rag"
+URL="http://192.168.47.131:8001/rag"
 DATA='{"query": "Which animals can hover in the air?"}'
 HEADER="Content-Type: application/json"
 
 # Number of concurrent requests to test
-CONCURRENCY_LEVELS=(1 5 10 20)
-ROUNDS=5  # How many batches to run at each concurrency level
+CONCURRENCY_LEVELS=(32)
+ROUNDS=20  # How many batches to run at each concurrency level
 
 for CONCURRENCY in "${CONCURRENCY_LEVELS[@]}"; do
   echo "Testing $CONCURRENCY concurrent requests, $ROUNDS rounds"
+  TOTAL_TIME=0
 
   for ((round = 1; round <= ROUNDS; round++)); do
     echo "  Round $round"
-    START_TIME=$(date +%s)
+    START_TIME=$(date +%s%N | cut -b1-13)
 
     for ((i = 1; i <= CONCURRENCY; i++)); do
       curl -s -o /dev/null -w "%{http_code} " -X POST "$URL" \
@@ -21,11 +22,15 @@ for CONCURRENCY in "${CONCURRENCY_LEVELS[@]}"; do
     done
 
     wait  # Wait for all concurrent requests to finish
-    END_TIME=$(date +%s)
+    END_TIME=$(date +%s%N | cut -b1-13)
     ELAPSED=$((END_TIME - START_TIME))
+    TOTAL_TIME=$((TOTAL_TIME+ELAPSED))
     echo "Finished $CONCURRENCY concurrent requests in $ELAPSED seconds"
     echo  # New line after response codes
   done
+
+  AVERAGE=$((TOTAL_TIME/ROUNDS))
+  echo "Average batch takes $AVERAGE seconds"
 
   echo "-----------------------------------------"
 done
--- task-2/serving_rag.py
+++ task-2/serving_rag.py
@@ -42,7 +42,7 @@ chat_pipeline = pipeline("text-generation", model=chat_model_path)
 # 2. Initialize a background thread to process the request (via calling the rag_pipeline function)
 # 3. Modify the predict function to put the request in the queue, instead of processing it immediately
 
-MAX_BATCH_SIZE = 10
+MAX_BATCH_SIZE = 8
 MAX_WAITING_TIME = 1
 request_queue = []
 response_queue = []
@@ -168,4 +168,4 @@ def predict(payload: QueryRequest):
     # }
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8001)
